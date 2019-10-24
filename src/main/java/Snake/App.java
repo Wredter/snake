06364dcc8ce5a94cfda580f0a8dc7ae7e1115480
Wiktor <wiktor.muraszko@wp.pl>
2019-10-24 23:56:18 +0200

    TODO

--- src/main/java/Snake/App.java
+++ src/main/java/Snake/App.java
@@ -10,6 +10,7 @@ public class App
 {
     public static void main( String[] args )
     {
+        //TODO add gameclock, add board loader, add rewards, add board restrictions
         Game game = new Game(3,3,'x','X',3,15,15);
 
         Scanner in = new Scanner(System.in);
