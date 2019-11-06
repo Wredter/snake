52516335c2f816389c1afa06f843b46bdc1338ef
Wiktor <wiktor.muraszko@wp.pl>
2019-11-06 20:22:03 +0100

    Simple timer, input is the problem

--- src/main/java/Snake/App.java
+++ src/main/java/Snake/App.java
@@ -2,20 +2,43 @@ package Snake;
 
 import Snake.Logic.Game;
 
+import java.math.BigInteger;
 import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class App
 {
+    static boolean doIhaveToUpdate;
+    static Game game;
     public static void main( String[] args )
     {
+        TimerTask updater = new TimerTask() {
+            @Override
+            public void run() {
+                    game.update('d');
+
+                if(!game.gameState){
+                    doIhaveToUpdate = false;
+                }
+            }
+        };
         //TODO add gameclock, add board loader
-        Game game = new Game(3,3,'x','X',3,15,15);
-        Scanner in = new Scanner(System.in);
-        String input = "";
-        while (!input.equals("q")){
-            input = in.next();
-            game.update(input.toCharArray()[0]);
-            if(!game.gameState){
+        game = new Game(3,3,'x','X',3,15,15);
+        Timer t = new Timer();
+        doIhaveToUpdate = true;
+        t.schedule(updater,900,1000);
+        while (true){
+            try
+            {
+                Thread.sleep(1000);
+            }
+            catch(InterruptedException ex)
+            {
+                Thread.currentThread().interrupt();
+            }
+            if(!doIhaveToUpdate) {
+                t.cancel();
                 return;
             }
         }
