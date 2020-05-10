3cb68def54d0d02c6b99d841ec6dd0dd6adc803b
Wiktor <wiktor.muraszko@wp.pl>
2020-05-10 12:33:06 +0200

    input is not working

--- src/main/java/Snake/App.java
+++ src/main/java/Snake/App.java
@@ -3,6 +3,9 @@ package Snake;
 import Snake.Logic.Game;
 
 import java.math.BigInteger;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Scanner;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -11,36 +14,58 @@ public class App
 {
     static boolean doIhaveToUpdate;
     static Game game;
+    static Scanner scanner;
+    static char input = 'd';
     public static void main( String[] args )
     {
-        TimerTask updater = new TimerTask() {
+        class GameUpdaterTask extends TimerTask{
+
+            char kb = 'd';
+            boolean doIHaveToUpdate;
+            Game game;
+            public GameUpdaterTask(Game game, boolean state){
+                this.game = game;
+                doIhaveToUpdate = state;
+
+            }
             @Override
             public void run() {
-                    game.update('d');
-
+                game.update(kb);
                 if(!game.gameState){
                     doIhaveToUpdate = false;
+                    cancel();
                 }
             }
-        };
-        //TODO add gameclock, add board loader
-        game = new Game(3,3,'x','X',3,15,15);
-        Timer t = new Timer();
-        doIhaveToUpdate = true;
-        t.schedule(updater,900,1000);
-        while (true){
-            try
-            {
-                Thread.sleep(1000);
-            }
-            catch(InterruptedException ex)
-            {
-                Thread.currentThread().interrupt();
-            }
-            if(!doIhaveToUpdate) {
-                t.cancel();
-                return;
+
+            public void setKb(char kb) {
+                this.kb = kb;
             }
         }
+        //TODO add gameclock, add board loader
+        doIhaveToUpdate = true;
+        game = new Game(3,3,'x','X',3,15,15);
+        GameUpdaterTask updater = new GameUpdaterTask(game, doIhaveToUpdate);
+        Timer timer = new Timer("Timer");
+        timer.schedule(updater,0, 1000);
+        scanner = new Scanner(System.in);
+
+
+        while (doIhaveToUpdate){
+            switch (input = scanner.nextLine().charAt(0)) {
+                case 'a':
+                    updater.setKb('a');
+                    break;
+                case 'd':
+                    updater.setKb('d');
+                    break;
+                case 'w':
+                    updater.setKb('w');
+                    break;
+                case 's':
+                    updater.setKb('s');
+                    break;
+            }
+        }
+
     }
 }
